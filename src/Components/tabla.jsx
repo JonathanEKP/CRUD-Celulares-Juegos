83e0a9e49a41b900d3fc1acb3921d3278630bf5d
JonathanEKP <jonathanvaldez2001@gmail.com>
2023-03-07 17:44:53 -0600

    Agregando responsividad a la tabla

--- src/Components/tabla.jsx
+++ src/Components/tabla.jsx
@@ -10,49 +10,53 @@ function Tabla({ filas, campos, controlador }) {
 
   return (
     <div className="container mt-3">
-      <table className="table">
-        <thead>
-          <tr >
-            <td colSpan={3}>
-              <Link to={`/${controlador}/Add`}>
-                <BotonGeneral>
-                  <AiOutlineUserAdd />
-                  Agregar
-                </BotonGeneral>
-              </Link>
-            </td>
-          </tr>
-          <tr className="table-secondary">
-            {campos.map((value, index) => {
-              return <th key={index}>{value}</th>;
-            })}
-          </tr>
-        </thead>
+      <div className="table-responsive">
+        <table className="table">
+          <thead>
+            <tr>
+              <td colSpan={3}>
+                <Link to={`/${controlador}/Add`}>
+                  <BotonGeneral>
+                    <AiOutlineUserAdd />
+                    Agregar
+                  </BotonGeneral>
+                </Link>
+              </td>
+            </tr>
+            <tr className="table-secondary">
+              {campos.map((value, index) => {
+                return <th key={index}>{value}</th>;
+              })}
+            </tr>
+          </thead>
 
-        <tbody>
-          {filas.map((value, index) => {
-            return (
-              <tr key={index}>
-                {Object.values(value).map((value2, index2) => {
-                  return <td key={index2}>{value2}</td>;
-                })}
-                <td>
-                  <Link to={`/${controlador}/Edit/${Object.values(value)[0]}`}>
-                    <BotonEditar />
-                  </Link>
-                </td>
-                <td>
-                  <Link
-                    to={`/${controlador}/Delete/${Object.values(value)[0]}`}
-                  >
-                    <BotonEliminar />
-                  </Link>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+          <tbody>
+            {filas.map((value, index) => {
+              return (
+                <tr key={index}>
+                  {Object.values(value).map((value2, index2) => {
+                    return <td key={index2}>{value2}</td>;
+                  })}
+                  <td>
+                    <Link
+                      to={`/${controlador}/Edit/${Object.values(value)[0]}`}
+                    >
+                      <BotonEditar />
+                    </Link>
+                  </td>
+                  <td>
+                    <Link
+                      to={`/${controlador}/Delete/${Object.values(value)[0]}`}
+                    >
+                      <BotonEliminar />
+                    </Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
